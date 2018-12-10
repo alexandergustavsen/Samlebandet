47f76135d4869ac079f871a49e1557aab7868c01
alexandergustavsen <alexander_gustavsen@hotmail.com>
2018-12-10 10:40:52 +0100

    la til home-screen og koblet til firebase

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+
 
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC472p6bon1WNU-l9uofXoeWp3sTppqJh0',
+      authDomain: 'samlebandet.firebaseapp.com',
+      databaseURL: 'https://samlebandet.firebaseio.com',
+      projectId: 'samlebandet',
+      storageBucket: 'samlebandet.appspot.com',
+      messagingSenderId: '693041379005'
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
