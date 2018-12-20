405638175592f050269ed66c7810209e4323a0a0
KristianJonsi <kriss963@hotmail.com>
2018-12-20 13:20:20 +0100

    list

--- src/screens/listGroupScreen.js
+++ src/screens/listGroupScreen.js
@@ -1,59 +1,92 @@
 import React, {Component} from 'react';
-import { StyleSheet, StatusBar, Text, View, ListView, TouchableOpacity } from 'react-native';
+import { List, StyleSheet, StatusBar, Text, View, ListView, TouchableOpacity, FlatList } from 'react-native';
 //import { Container, Content, Header, Form, Input, Item, Button, Label, Icon, List, ListItem } from 'native-base';
+import * as firebase from 'firebase'
 
-var data = []
 
                         //tittel
 //kategori/bilde        //sted   //tid
                         //deltagere
 
 export default class ListGroupScreen extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            //flatListData: []
+            title: '',
+            time: '',
+            size: '',
+            desc: '',
+            cate: '',
+            place: '',
+        }
+        
+    }
+
+    componentDidMount() {
+        var that = this
+    
+        firebase.database().ref('/groups').on('child_added', function (data){
+        
+        
+        that.setState({
+            title: data.val().groupTitle,
+            time: data.val().groupTime,
+            size: data.val().groupSize,
+            desc: data.val().groupDesc,
+            cate: data.val().groupCate,
+            place: data.val().groupPlace,
+        })
+
+        /*
+        var newData = [...this.state.flatListData]
+        newData.push(data)
+        that.setState({flatListData : newData})
+        */
+    
+        //console.log(data)
+    
+        })
+
+        //console.log(that.state.test)
+
+        //console.log(that.state.test)
+    }
+
     render() {
+        console.log(this.state.title)
+        console.log(this.state.time)
+        console.log(this.state.size)
+        console.log(this.state.desc)
+        console.log(this.state.cate)
+        console.log(this.state.place)
         return (
-            <View style={styles.container}>
-                <View style={styles.testingStuff}>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('MakeGroup')} style={styles.buttonContainer}>
-                        <Text style={styles.buttonText}>Make Group</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Chat')} style={styles.buttonContainer}>
-                        <Text style={styles.buttonText}>Chat</Text>
-                    </TouchableOpacity>
-                </View>
+            <View>
+                <Text>{this.state.title}</Text>
+                <Text>{this.state.time}</Text>
+                <Text>{this.state.size}</Text>
+                <Text>{this.state.desc}</Text>
+                <Text>{this.state.cate}</Text>
+                <Text>{this.state.place}</Text>
             </View>
-            );
+        );
     }
 }
 
 const styles = StyleSheet.create({
-
     container: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
     },
-
+    /*
     groupContainer: {
-        padding: 10,
-        flex: 1,
-        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-
-    box1: {
-        flex: 1,
-        //backgroundColor: 'blue',
-    },
-    box2: {
-        flex: 1,
-        flexDirection: 'coloumn',
-        //backgroundColor: 'yellow',
-    },
-
-    textStyle: {
-        textAlign: 'center',
-    },
-
-    testingStuff: {
-
-    },
+    */
 
     buttonContainer: {
         backgroundColor: '#2980b9',
@@ -61,9 +94,9 @@ const styles = StyleSheet.create({
     },
 
     buttonText: {
-        textAlign: 'center',
-        color: '#FFFFFF',
-        fontWeight: '700',
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: '700',
     },
 });
 
@@ -75,4 +108,16 @@ const styles = StyleSheet.create({
     <Text> Primary </Text>
 </Button>
 </Item>
+
+
+
+View style={styles.testingStuff}>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('MakeGroup')} style={styles.buttonContainer}>
+                        <Text style={styles.buttonText}>Make Group</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Chat')} style={styles.buttonContainer}>
+                        <Text style={styles.buttonText}>Chat</Text>
+                    </TouchableOpacity>
+                </View>
+
 */
