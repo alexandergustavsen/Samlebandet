1d61be201c98d76f47178a929958bf9660de91f7
alexandergustavsen <alexander_gustavsen@hotmail.com>
2019-05-14 11:50:50 +0200

    listGroupScreen update

--- src/screens/listGroupScreen.js
+++ src/screens/listGroupScreen.js
@@ -1,19 +1,13 @@
-import React, {Component} from 'react';
-import { List, StyleSheet, StatusBar, Text, View, ListView, TouchableOpacity, FlatList } from 'react-native';
-//import { Container, Content, Header, Form, Input, Item, Button, Label, Icon, List, ListItem } from 'native-base';
+import React, { Component } from 'react';
+import { StyleSheet, StatusBar, Text, View, ListView, TouchableOpacity, FlatList } from 'react-native';
+import { List, ListItem } from "native-base";
 import * as firebase from 'firebase'
-
-
-                        //tittel
-//kategori/bilde        //sted   //tid
-                        //deltagere
 
 export default class ListGroupScreen extends Component {
     constructor(){
-        super()
+        super();
 
         this.state = {
-            //flatListData: []
             title: '',
             time: '',
             size: '',
@@ -25,51 +19,33 @@ export default class ListGroupScreen extends Component {
     }
 
     componentDidMount() {
-        var that = this
-    
+        let that = this;
+
         firebase.database().ref('/groups').on('child_added', function (data){
-        
-        
-        that.setState({
-            title: data.val().groupTitle,
-            time: data.val().groupTime,
-            size: data.val().groupSize,
-            desc: data.val().groupDesc,
-            cate: data.val().groupCate,
-            place: data.val().groupPlace,
+
+            that.setState({
+                /*title: data.val().groupTitle,
+                time: data.val().groupTime,
+                size: data.val().groupSize,
+                desc: data.val().groupDesc,
+                cate: data.val().groupCate,
+                place: data.val().groupPlace,*/
+
+                data: data.val()
+
+            })
+
         })
-
-        /*
-        var newData = [...this.state.flatListData]
-        newData.push(data)
-        that.setState({flatListData : newData})
-        */
-    
-        //console.log(data)
-    
-        })
-
-        //console.log(that.state.test)
-
-        //console.log(that.state.test)
     }
 
     render() {
-        console.log(this.state.title)
-        console.log(this.state.time)
-        console.log(this.state.size)
-        console.log(this.state.desc)
-        console.log(this.state.cate)
-        console.log(this.state.place)
+
+        console.log(this.state.data);
+
         return (
-            <View>
-                <Text>{this.state.title}</Text>
-                <Text>{this.state.time}</Text>
-                <Text>{this.state.size}</Text>
-                <Text>{this.state.desc}</Text>
-                <Text>{this.state.cate}</Text>
-                <Text>{this.state.place}</Text>
-            </View>
+            <List>
+                <Text>{this.state.groupTitle}</Text>
+            </List>
         );
     }
 }
@@ -81,12 +57,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    /*
-    groupContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    */
 
     buttonContainer: {
         backgroundColor: '#2980b9',
@@ -99,25 +69,3 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     },
 });
-
-/*
-<Item>
-<Button 
-    primary 
-    onPress={() => this.props.navigation.navigate('MakeGroup')}>
-    <Text> Primary </Text>
-</Button>
-</Item>
-
-
-
-View style={styles.testingStuff}>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('MakeGroup')} style={styles.buttonContainer}>
-                        <Text style={styles.buttonText}>Make Group</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Chat')} style={styles.buttonContainer}>
-                        <Text style={styles.buttonText}>Chat</Text>
-                    </TouchableOpacity>
-                </View>
-
-*/
