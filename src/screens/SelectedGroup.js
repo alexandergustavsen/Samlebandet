aca892dcaf23b60ba313e3f504398c7948c14c0a
alexandergustavsen <alexander_gustavsen@hotmail.com>
2019-05-25 14:35:25 +0200

    selected group

---
+++ src/screens/SelectedGroup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Text, View} from "react-native";
+
+export default class Profile extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>{this.props.name}</Text>
+                <Text>{this.props.time}</Text>
+                <Text>{this.props.desc}</Text>
+                <Text>{this.props.size}</Text>
+                <Text>{this.props.place}</Text>
+                <Text>{this.props.cate}</Text>
+            </View>
+        );
+    }
+}
