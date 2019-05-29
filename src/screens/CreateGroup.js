3383c6477cce65a7100382490a53fb695bdecb43
alexandergustavsen <alexander_gustavsen@hotmail.com>
2019-05-29 16:28:08 +0200

    mainly design changes to creategroup.js

--- src/screens/CreateGroup.js
+++ src/screens/CreateGroup.js
@@ -3,6 +3,7 @@ import { StyleSheet, StatusBar, Text, View, ListView, TouchableOpacity, Picker, 
 import { Container, Content, Header, Form, Input, Item, Button, Label, Icon, List, ListItem } from 'native-base';
 
 import DateTimePicker from 'react-native-modal-datetime-picker';
+import simplePicker from 'react-native-simple-modal-picker';
 import moment from 'moment';
 import * as firebase from 'firebase';
 
@@ -23,6 +24,7 @@ export default class CreateGroup extends Component {
       isVisible: false,
       isVisibleAntall: false,
       PickerSize: '',
+      PickerSize2: '',
       PickerCate: ''
     }
   }
@@ -31,7 +33,7 @@ export default class CreateGroup extends Component {
         title: 'Opprett gruppe',
         headerTitleStyle: ({
             color: '#383838',
-            fontWeight: 'regular',
+            fontWeight: 'normal',
             fontSize: 20
         }),
         headerStyle: ({
@@ -80,133 +82,187 @@ export default class CreateGroup extends Component {
     });
     firebase.database().ref('/users/' + userId + '/groups/').push({
       groupId: key
-    })
+    });
     this.props.navigation.navigate('Home');
   } 
 
+  showActionSheet = () => {
+    this.ActionSheet.show()
+  };
+
+  showActionSheet2 = () => {
+    this.ActionSheet2.show()
+  };
+
   render() {
     return (
-      <Container style={styles.container}>
+      <View style={styles.container}>
         <View style = {styles.image}>
           <Icon
-            
+            name='camera'
+            size={50}
           />
-          <Text>Legg til et bilde</Text>
+          <Text style={{fontSize: 15}}>Legg til bilde</Text>
         </View>
-        <View style={styles.input}>
-          <Input 
-            onChangeText={(groupTitle) => this.setState({groupTitle})} 
-            placeholder="Tittel"
-          />
-        </View>
-        <View style={styles.input}>
-          <Input 
-            onChangeText={(groupDesc) => this.setState({groupDesc})} 
-            placeholder="Beskrivelse"
-          />  
-        </View>
-        <View style={styles.item}>
-          <TouchableOpacity style={styles.button} onPress={this.showPicker}>
-            <Text style={styles.text}>Show DatePicker</Text>
-          </TouchableOpacity>
-          <DateTimePicker 
-            cancelTextIOS={'Exit'}
-            confirmTextIOS={'OK'}
-            cancelTextStyle={{color: 'red', fontSize: 20}}
-            confirmTextStyle={{color: 'blue', fontSize: 20}}
+        <View style={{flex: 6, borderTopWidth: 1.5, borderColor: '#bbb', marginLeft: 25, marginRight: 25, marginTop: 30}}>
+          <View style={styles.items}>
+            <View style={{flex: 1}}>
+              <Text style={{color: '#383838', fontWeight: 'bold'}}>Navn</Text>
+            </View>
+            <View style={{flex: 2}}>
+              <Input
+              style={{color: '#747475', fontSize: 15}}
+              onChangeText={(groupTitle) => this.setState({groupTitle})}
+              placeholder="Gi gruppen et navn"
+              />
+            </View>
+          </View>
+          <View style={{
+              flex: 0.4,
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              borderBottomWidth: 1,
+              borderColor: '#ccc',
+              marginTop: 25
+          }}>
+              <View style={{flex: 1}}>
+                  <Text style={{marginTop: 5, color: '#383838', fontWeight: 'bold'}}>Beskrivelse</Text>
+              </View>
+              <View style={{flex: 2}}>
+                  <Input
+                      style={{color: '#747475', fontSize: 15}}
+                      multiline={true}
+                      onChangeText={(groupDesc) => this.setState({groupDesc})}
+                      placeholder="Legg til en beskrivelse..."
+                  />
+              </View>
+          </View>
+          <View style={styles.items}>
+              <View style={{flex: 1}}>
+                  <Text style={{color: '#383838', fontWeight: 'bold'}}>Sted</Text>
+              </View>
+              <View style={{flex: 2}}>
+                  <Input
+                      style={{color: '#747475', fontSize: 15}}
+                      onChangeText={(groupPlace) => this.setState({groupPlace})}
+                      placeholder="Hvor skal det være?"
+                  />
+              </View>
+          </View>
+          <View style={styles.items}>
+              <View style={{flex: 1}}>
+                  <Text style={{color: '#383838', fontWeight: 'bold'}}>Tidspunkt</Text>
+              </View>
+              <View style={{flex: 2}}>
+                  <TouchableOpacity style={{marginLeft: 4}} onPress={this.showPicker}>
+                      <Text style={{color: '#747475', fontSize: 15}}>Når begynner det?</Text>
+                  </TouchableOpacity>
+                  <DateTimePicker
+                      cancelTextIOS={'Exit'}
+                      confirmTextIOS={'OK'}
+                      cancelTextStyle={{color: 'red', fontSize: 20}}
+                      confirmTextStyle={{color: 'blue', fontSize: 20}}
 
-            isVisible={this.state.isVisible}
-            onConfirm={this.handlePicker}
-            onCancel={this.hidePicker}
-            mode={'datetime'}
-            is24Hour={true}
-          />
+                      isVisible={this.state.isVisible}
+                      onConfirm={this.handlePicker}
+                      onCancel={this.hidePicker}
+                      mode={'datetime'}
+                      is24Hour={true}
+                      />
+              </View>
+          </View>
+          <View style={styles.items}>
+              <View style={{flex: 1}}>
+                  <Text style={{color: '#383838', fontWeight: 'bold'}}>Maks Antall</Text>
+              </View>
+              <View style={{flex: 2}}>
+                  <Text style={{marginLeft: 4, color: '#747475', fontSize: 15}}>{}</Text>
+
+                  {/*<ActionSheet
+                      ref={o => this.ActionSheet = o}
+                      title={'Velg gruppestørrelse'}
+                      options={[2, 3, 4, 5, 6, 7, 8,'Cancel']}
+                      cancelButtonIndex={8}
+                      destructiveButtonIndex={8}
+                      onPress={(index) => {this.setState({PickerSize: index, groupSize: index+2})
+                      }}/>*/}
+              </View>
+          </View>
+          <View style={{
+              flex: 0.3,
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              borderBottomWidth: 1.5,
+              borderColor: '#bbb'
+          }}>
+              <View style={{flex: 1}}>
+                  <Text style={{color: '#383838', fontWeight: 'bold'}}>Kategori</Text>
+              </View>
+              <View style={{flex: 2}}>
+                  <Text style={{marginLeft: 4, color: '#747475', fontSize: 15}} onPress={this.showActionSheet2}>{}</Text>
+                  {/*<ActionSheet
+                    ref={o => this.ActionSheet2 = o}
+                    title={'Velg en interesse'}
+                    options={['Mat', 'Uteliv', 'Friluft', 'Trening', 'Opplevelser', 'Hobby', 'Underholdning', 'Diverse', 'Cancel']}
+                    cancelButtonIndex={8}
+                    destructiveButtonIndex={8}
+                    onPress={(index) => {this.setState({PickerSize2: index, groupCate: index})
+                    }}/>*/}
+              </View>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 60}}>
+            <Button style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#fff',
+                borderColor: '#000',
+                borderWidth: 1,
+                borderRadius: 30,
+                width: 250,
+                height: 50
+            }}
+              onPress={() => this.addGroup(
+              this.state.groupTitle,
+              this.state.groupDesc,
+              this.state.groupTime,
+              this.state.groupPlace,
+              this.state.groupSize,
+              this.state.groupCate
+              )}>
+              <Text style={{fontSize: 20}}>Opprett</Text>
+            </Button>
+          </View>
         </View>
-        <View style={styles.input}>
-          <Input 
-            onChangeText={(groupPlace) => this.setState({groupPlace})} 
-            placeholder="Sted"
-          />
-        </View>
-        <View style={styles.item}>
-          <Picker style={{width:'80%'}}
-          selectedValue={this.state.PickerSize}
-          onValueChange={(itemValue, itemIndex) => this.setState({PickerSize:itemValue, groupSize: itemValue})}
-          >
-            <Picker.Item label="Antall" value=""/>
-            <Picker.Item label="2" value="2" />
-            <Picker.Item label="3" value="3" />
-            <Picker.Item label="4" value="4" />
-            <Picker.Item label="5" value="5" />
-            <Picker.Item label="6" value="6" />
-            <Picker.Item label="7" value="7" />
-            <Picker.Item label="8" value="8" />
-          </Picker>
-        </View>
-        <View style={styles.item}>
-          <Picker style={{width:'80%'}}
-          selectedValue={this.state.PickerCate}
-          onValueChange={(itemValue, itemIndex) => this.setState({PickerCate:itemValue, groupCate: itemValue})}
-          >
-            <Picker.Item label="Kategori" value=""/>
-            <Picker.Item label="Mat" value="Mat" />
-            <Picker.Item label="Uteliv" value="Uteliv" />
-            <Picker.Item label="Friluft" value="Friluft" />
-            <Picker.Item label="Trening" value="Trening" />
-            <Picker.Item label="Opplevelser" value="Opplevelser" />
-            <Picker.Item label="Hobby" value="Hobby" />
-            <Picker.Item label="Underholdning" value="Underholdning" />
-            <Picker.Item label="Diverse" value="Diverse" />
-          </Picker>
-        </View>
-        <View style={styles.item}>
-          <Button onPress={() => this.addGroup(
-            this.state.groupTitle, 
-            this.state.groupDesc, 
-            this.state.groupTime,
-            this.state.groupPlace,
-            this.state.groupSize,
-            this.state.groupCate
-            )}>
-            <Icon name="add" />
-          </Button>
-        </View>
-      </Container>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   image: {
-    flex: 3,
-    backgroundColor: '#999',
+    flex: 1,
+    backgroundColor: '#ddd',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    padding: 10,
     flexDirection: 'column'
-  },
-  item: {
-    flex: 5,
-  },
-  input: {
-    flex: 1,
-    borderBottomWidth: 0.5,
-  },
-  button: {
-    width: 250,
-    height: 50,
-    backgroundColor: '#BF3000',
-    borderRadius: 30,
-    justifyContent: 'center',
-    marginTop: 15
   },
   text: {
     fontSize: 18,
     color: 'white',
     textAlign: 'center'
+  },
+  items: {
+      flex: 0.3,
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      borderBottomWidth: 1,
+      borderColor: '#ccc'
   }
 });
