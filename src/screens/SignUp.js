82e0ebafcd61bbce0af4040f5e07599f82fcaeb3
alexandergustavsen <alexander_gustavsen@hotmail.com>
2019-06-05 17:11:31 +0200

    design changes to header

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -159,9 +159,7 @@ export default class SignUp extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        flex: 1
     },
     button: {
         justifyContent: 'center',
